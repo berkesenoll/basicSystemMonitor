3ebbea90d0e168267852cae47a1cbbaf15d82488
Berke <berkesenol@protonmail.com>
2026-08-23 23:38:59 +0300

    a little decoration and make sure the shell is bash.

--- firstProject.sh
+++ firstProject.sh
@@ -1,6 +1,9 @@
-echo 'basic system tracking'
+#!/usr/bin/env bash 
+#use bash shell if not using already.
 
+if [[ $(id -u) -eq 0 ]]; then
 echo '
+basic system tracking
 1 memory usage
 2 disk usage
 3 uptime
@@ -9,30 +12,26 @@ echo '
 
 read -p "Please choose one: " choiceOne
 
-if [[ $(id -u) -eq 0 ]]; then
-
-if [[ $choiceOne -eq 2 ]]; then #3 disk usage
-    df -h
-fi
-
-if [[ $choiceOne -eq 3 ]]; then
-    uptimeSec=$(awk '{print $1}' /proc/uptime)
-    uptimeHour=$((uptimeSec/3600))
-    echo "uptime: $uptimeSec Seconds / $uptimeHour Hours" 
-fi
-
 if [[ $choiceOne -eq 1 ]]; then
     memTotal=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
     memAvailable=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
     memUse=$((memTotal-memAvailable))
     memPerc=$((memUse*100/memTotal))
     echo "Memory usage: $memPerc%"
-fi
 
-if [[ $choiceOne -eq 4 ]]; then
+elif [[ $choiceOne -eq 2 ]]; then #3 disk usage
+    df -h
+
+elif [[ $choiceOne -eq 3 ]]; then
+    uptimeSec=$(awk '{print $1}' /proc/uptime)
+    echo "uptime: $uptimeSec Seconds"
+
+elif [[ $choiceOne -eq 4 ]]; then
     ls /proc | grep "^[0-9]\+$" | wc -l
 fi
 
+else
+    echo "You must be superuser to execute this program!"
 fi
 
 
