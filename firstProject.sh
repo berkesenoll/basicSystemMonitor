ff02cde9021293b999a053da56659af1d3cb2c31
Berke <berkesenol@protonmail.com>
2026-08-24 22:20:56 +0300

    added functions

--- firstProject.sh
+++ firstProject.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash 
 #use bash shell if not using already.
 
+function memory_usage(){
+    local memTotal
+    local memAvailable
+    local memUse
+    local memPerc
+    memTotal=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+    memAvailable=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
+    memUse=$((memTotal-memAvailable))
+    memPerc=$((memUse*100/memTotal))
+
+    echo "Memory usage: $memPerc%"
+}
+
+function disk_usage(){
+    df -h
+}
+
+function process_amount(){
+    ls /proc | grep "^[0-9]\+$" | wc -l
+}
+
+function report_uptime(){
+    local uptimeSec
+    uptimeSec=$(awk '{print $1}' /proc/uptime)
+    echo "uptime: $uptimeSec Seconds"
+}
+
 if [[ $(id -u) -eq 0 ]]; then
 echo '
 basic system tracking
@@ -13,21 +40,16 @@ basic system tracking
 read -p "Please choose one: " choiceOne
 
 if [[ $choiceOne -eq 1 ]]; then
-    memTotal=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
-    memAvailable=$(awk '/MemAvailable/ {print $2}' /proc/meminfo)
-    memUse=$((memTotal-memAvailable))
-    memPerc=$((memUse*100/memTotal))
-    echo "Memory usage: $memPerc%"
+    memory_usage
 
-elif [[ $choiceOne -eq 2 ]]; then #3 disk usage
-    df -h
+elif [[ $choiceOne -eq 2 ]]; then
+    disk_usage
 
 elif [[ $choiceOne -eq 3 ]]; then
-    uptimeSec=$(awk '{print $1}' /proc/uptime)
-    echo "uptime: $uptimeSec Seconds"
+    report_uptime
 
 elif [[ $choiceOne -eq 4 ]]; then
-    ls /proc | grep "^[0-9]\+$" | wc -l
+    process_amount
 fi
 
 else
